f555a307fff4a962a20d4df3b081e0378216e84a
mouhStf <mouhamadou.mstf@gmail.com>
2026-02-24 12:07:00 +0000

    Redirecting contact for to mikemikedelta.alwaysdata.net/api/contact

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const payload = Object.fromEntries(formData.entries());
 
             try {
-                const res = await fetch('/api/contact', {
+                const res = await fetch('https://mikemikedelta.alwaysdata.net/api/contact', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify(payload),
